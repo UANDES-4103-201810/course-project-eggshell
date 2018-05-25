c7e40ebd7ecc1236048dd10ab88c5a298785364c
Web <cerenchun@miuandes.cl>
2018-05-24 22:33:19 -0400

    intento profile image

--- app/models/project.rb
+++ app/models/project.rb
@@ -8,7 +8,7 @@ class Project < ApplicationRecord
   validates :goal,  numericality: {:greater_than_or_equal_to => 0}
   validates :money_colected,  numericality: {:greater_than_or_equal_to => 0}
   validates :name, length: {in: 2..20}
-  validates :description, length: {:minimum => 20}
+  validates :description, length: {:minimum => 1}
   #validates :category, length: {:minimum => 2}
   before_save  do start_later end
   before_save do state_validation end
